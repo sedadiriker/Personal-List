f99cb033a9da8e330ca0301470560dd0e25b67a1
Seda <seda.diriker@gmail.com>
2024-08-13 23:22:42 +0300

    Personal class created(model)

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Personal(models.Model):
+    id = models.AutoField(primary_key=True) 
+    firstname = models.CharField(max_length = 100)
+    lastname = models.CharField(max_length = 100)
+    email = models.EmailField()
+    position = models.CharField(max_length = 50)
+
+def __str__(self):
+    return f"{self.firstname} {self.lastname}"
