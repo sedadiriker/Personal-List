860a233a193223a053522f095c53d1daa1ca6a92
seda <seda.diriker@gmail.comclear>
2024-08-14 09:33:55 +0300

    viewset created

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Personal
+from .serializer import PersonalSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class PersonalViewset(viewsets.ModelViewSet):
+    queryset = Personal.objects.all()
+    serializer_class = PersonalSerializer
